6fcb0d591dbb9a6408994f8d6d6055b57cc0f38b
Aki Van Ness <aki@lethalbit.net>
2022-07-03 10:43:54 -0400

    camera: updated wrapper to be a touch more complete

--- src/camera.rs
+++ src/camera.rs
@@ -23,16 +23,81 @@ mod binding {
 
 pub struct Inochi2DCamera {
 	handle: binding::InCameraPtr,
+	zoom: f32,
+	x: f32,
+	y: f32,
 }
 
 
 impl Inochi2DCamera {
-    pub fn new() -> Self {
-        unsafe {
+	pub fn set_zoom(&mut self, zoom: f32) {
+		unsafe {
+			binding::inCameraSetZoom(self.handle, zoom);
+		}
+
+		self.zoom = zoom;
+	}
+
+	pub fn get_zoom(&mut self) -> f32 {
+		let mut _z: f32 = 0.0;
+		unsafe {
+			binding::inCameraGetZoom(self.handle, &mut _z);
+		}
+
+		self.zoom = _z;
+		_z
+	}
+
+	pub fn sett_pos(&mut self, x: f32, y: f32) {
+		unsafe {
+			binding::inCameraSetPosition(self.handle, x, y);
+		}
+
+		self.x = x;
+		self.y = y;
+	}
+
+	pub fn get_pos(&mut self) -> (f32, f32) {
+		let mut _x: f32 = 0.0;
+		let mut _y: f32 = 0.0;
+
+		unsafe {
+			binding::inCameraGetPosition(self.handle, &mut _x, &mut _y);
+		}
+
+		self.x = _x;
+		self.y = _y;
+
+		(_x, _y)
+	}
+
+    pub fn new(zoom: Option<f32>, x: Option<f32>, y: Option<f32>) -> Self {
+		unsafe {
             let hndl = binding::inCameraGetCurrent();
+			let cam_zoom = zoom.unwrap_or_else(|| {
+				let mut _z: f32 = 0.0;
+				binding::inCameraGetZoom(hndl, &mut _z);
+				_z
+			});
+			// TODO: find a better way to do this
+			let cam_x = x.unwrap_or_else(|| {
+				let mut _x: f32 = 0.0;
+				let mut _y: f32 = 0.0;
+				binding::inCameraGetPosition(hndl, &mut _x, &mut _y);
+				_x
+			});
+			let cam_y = y.unwrap_or_else(|| {
+				let mut _x: f32 = 0.0;
+				let mut _y: f32 = 0.0;
+				binding::inCameraGetPosition(hndl, &mut _x, &mut _y);
+				_y
+			});
 
 			Inochi2DCamera {
                 handle: hndl,
+				zoom: cam_zoom,
+				x: cam_x,
+				y: cam_y,
             }
         }
     }
