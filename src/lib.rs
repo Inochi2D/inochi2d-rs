d54d10353e4e4cafb0982cf8bf2b49db403565ef
Mary <mary@mary.zone>
2022-07-05 09:26:59 +0200

    misc: Make it stable compatible
    This commit adds a new feature "nightly" to hides extern_types
    requirement behind a cfg!.
    
    When the feature isn't active, a repr(C) struct is defined with a size
    of 0 (with one of the old tricks around for that matter)
    
    I took the liberty to hide this boilerplate around a simple macro.
    
    This fix build on stable compiler.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
-#![feature(extern_types)]
-
+#![cfg_attr(feature = "nightly", feature(extern_types))]
 /*
     Copyright © 2022, Inochi2D Project
     Distributed under the 2-Clause BSD License, see LICENSE file.
@@ -11,6 +10,9 @@
     An attempted idiomatic wrapper for [Inochi2D](https://github.com/Inochi2D/inochi2d).
 
 */
+
+#[macro_use]
+mod macros;
 
 pub mod camera;
 pub mod core;
