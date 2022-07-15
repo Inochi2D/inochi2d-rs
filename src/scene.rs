6e5fbef6cfe5926ca3731722991d44e5ce9748ca
Aki Van Ness <aki@lethalbit.net>
2022-07-15 13:53:19 -0400

    Did a rough implementation of the Inochi2D scene wrapper

--- src/scene.rs
+++ src/scene.rs
@@ -13,4 +13,28 @@ use crate::ffi::{
 
 pub struct Inochi2DScene {}
 
-impl Inochi2DScene {}
+impl Inochi2DScene {
+    pub fn new() -> Self {
+        unsafe {
+            inSceneBegin();
+        }
+
+        Inochi2DScene {}
+    }
+
+    pub fn draw(&mut self, x: f32, y: f32, width: f32, height: f32){
+        unsafe {
+            inSceneEnd();
+            inSceneDraw(x, y, width, height);
+            inSceneBegin();
+        }
+    }
+}
+
+impl Drop for Inochi2DScene {
+    fn drop(&mut self) {
+        unsafe {
+            inSceneEnd();
+        }
+    }
+}
